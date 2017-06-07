124a6c224ad98fccbbb356452edcdc450de4155f
Ic3Venom <Ic3Venom@gmail.com>
2017-06-06 20:24:22 -0700

    Update bytebitbinary_test1.py

--- bytebitbinary_test1.py
+++ bytebitbinary_test1.py
@@ -1,9 +1,16 @@
-
-def read():
-    pass
-
 def main():
-    pass
+    range = raw_input('Length and width of image: ')
+    size = range.split(',')
+    img = Image.new('RGB', (size[0], size[1]), "black")
+    
+    #credit @https://en.wikibooks.org/wiki/Python_Imaging_Library/Editing_Pixels
+    for i in range(int(size[0])):
+        for j in range(int(size[0])):
+            pixels[i,j] = (i, j, 100)
+            
+    img.show()
+    
+    
 
 if __name__ == '__main__':
     main()
