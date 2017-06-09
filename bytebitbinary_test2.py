c6c6ac4c2da5783e434978294012dd6117fff7b1
Ic3Venom <Ic3Venom@gmail.com>
2017-06-08 20:44:56 -0700

    Add files via upload

---
+++ bytebitbinary_test2.py
@@ -0,0 +1,37 @@
+from PIL import Image
+
+def primes(maxRange):
+    numRange= range(3, maxRange +1, 2) #currently has composites, they will be removed @end of program 
+    primes = [2]
+    
+    while numRange: 
+        repeat = 1
+        guess = numRange[0] 
+        primes.append(numRange[0]) 
+        
+        while repeat * guess <= maxRange: 
+            
+            if repeat * guess in numRange: 
+                numRange.remove(repeat * guess) 
+            
+            repeat += 1 
+            
+    return primes
+
+def main():
+    size = 11
+    img = Image.new('RGB', (size,size), (240, 240, 240))
+    pixels = img.load()
+    prime = primes(size*size)
+    
+    for x in range(size):    
+        previous = x*size
+        for y in range(size):
+            if (x*size)+(y+1) in prime:
+                pixels[y,x] = (118, 223, 109)
+            
+    img.show()
+    
+
+if __name__ == '__main__':
+    main()
